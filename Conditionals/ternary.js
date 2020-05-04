e39b81462efa493afcc9dcfb89109963d1b62396
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-05 01:24:39 +0530

    Create ternary.js

---
+++ Conditionals/ternary.js
@@ -0,0 +1,28 @@
+let isLocked = false;
+
+// if (isLocked) {
+//   console.log('You will need a key to open the door.');
+// } else {
+//   console.log('You will not need a key to open the door.');
+// }
+
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+let isCorrect = true;
+
+// if (isCorrect) {
+//   console.log('Correct!');
+// } else {
+//   console.log('Incorrect!');
+// }
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!')
+
+let favoritePhrase = 'Love That!';
+
+// if (favoritePhrase === 'Love That!') {
+//   console.log('I love that!');
+// } else {
+//   console.log("I don't love that!");
+// }
+favoritePhrase==='Love That!' ? console.log('I love that!') : console.log("I don't love that!")
