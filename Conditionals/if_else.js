8aadc39a58e7f4dc86819654cc2c172c224b66ed
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-04 16:00:01 +0530

    Create if_else.js

---
+++ Conditionals/if_else.js
@@ -0,0 +1,14 @@
+let sale = true;
+
+sale = false;
+
+if(sale) {
+  console.log('Time to buy!');
+}
+else{
+  console.log('Time to wait for a sale.')
+}
+
+Uses the else keyword following the code block of an if statement.
+Has a code block that is wrapped by a set of curly braces {}.
+The code inside the else statement code block will execute when the if statement’s condition evaluates to false.
