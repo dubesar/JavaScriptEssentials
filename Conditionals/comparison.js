7a9c7eefec230ecc46f16304cc2291564e552fa5
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-04 16:05:14 +0530

    Update comparison.js

--- Conditionals/comparison.js
+++ Conditionals/comparison.js
@@ -6,3 +6,10 @@ if(hungerLevel>7){
 else{
   console.log('We can eat later!')
 }
+
+// less than: <
+// Greater than: >
+// Less than or equal to: <=
+// Greater than or equal to: >=
+// Is equal to: ===
+// Is NOT equal to: !==
