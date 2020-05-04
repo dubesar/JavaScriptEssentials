5943d299c2626ffa69563f853a7ac139cc845b5f
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-04 16:00:24 +0530

    Update if_else.js

--- Conditionals/if_else.js
+++ Conditionals/if_else.js
@@ -9,6 +9,6 @@ else{
   console.log('Time to wait for a sale.')
 }
 
-Uses the else keyword following the code block of an if statement.
-Has a code block that is wrapped by a set of curly braces {}.
-The code inside the else statement code block will execute when the if statement’s condition evaluates to false.
+// Uses the else keyword following the code block of an if statement.
+// Has a code block that is wrapped by a set of curly braces {}.
+// The code inside the else statement code block will execute when the if statement’s condition evaluates to false.
