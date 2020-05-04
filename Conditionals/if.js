75f2eaad00a30989f8a185ea1cec5eaab36b2fa0
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-04 15:55:29 +0530

    Update if.js

--- Conditionals/if.js
+++ Conditionals/if.js
@@ -3,3 +3,8 @@ sale = false
 if(sale){
   console.log('Time to buy!')
 }
+
+// The if keyword followed by a set of parentheses () which is followed by a code block, or block statement, indicated by a set of curly braces {}.
+// Inside the parentheses (), a condition is provided that evaluates to true or false.
+// If the condition evaluates to true, the code inside the curly braces {} runs, or executes.
+// If the condition evaluates to false, the block won’t execute.
