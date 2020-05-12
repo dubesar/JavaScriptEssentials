555e050e35e57b9a1b702933bf817e674f696786
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-13 01:44:13 +0530

    Create function_expression.js

---
+++ Functions/function_expression.js
@@ -0,0 +1,15 @@
+function weekday(day) {
+
+}
+
+const plantNeedsWater = function weekday(day) {
+  if(day === 'Wednesday'){
+    return true
+  }
+  else{
+    return false
+  }
+}
+
+plantNeedsWater('Tuesday')
+console.log(plantNeedsWater('Tuesday'))
