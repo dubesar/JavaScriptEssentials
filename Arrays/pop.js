f4bf07e604a7b488fa3c1374e54db918ce5cadee
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-14 23:30:22 +0530

    Create pop.js

---
+++ Arrays/pop.js
@@ -0,0 +1,5 @@
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+
+chores.pop()
+
+console.log(chores) //[ 'wash dishes', 'do laundry', 'take out trash', 'cook dinner' ]
