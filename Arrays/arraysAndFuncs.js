ba0452b1fdfa91bf64fe730afb1d3ddff59443f7
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-15 14:34:15 +0530

    Create arraysAndFuncs.js

---
+++ Arrays/arraysAndFuncs.js
@@ -0,0 +1,16 @@
+const concept = ['arrays', 'can', 'be', 'mutated'];
+
+function changeArr(arr){
+  arr[3] = 'MUTATED';
+}
+
+changeArr(concept);
+
+console.log(concept) // [ 'arrays', 'can', 'be', 'MUTATED' ]
+
+function removeElement(newArr) {
+  newArr.pop()
+}
+
+removeElement(concept)
+console.log(concept) //[ 'arrays', 'can', 'be' ]
