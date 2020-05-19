8e29334d07e9e97612de468753e9cc5b6f02d250
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-19 15:13:26 +0530

    Create bracketNotation.js

---
+++ Objects/bracketNotation.js
@@ -0,0 +1,12 @@
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  'Active Mission' : true,
+  homePlanet : 'Earth', 
+  numCrew: 5
+ };
+
+let propName =  'Active Mission';
+
+let isActive = spaceship['Active Mission']
+
+console.log(spaceship['Active Mission'])
