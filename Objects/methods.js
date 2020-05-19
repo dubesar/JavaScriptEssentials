a49d12d811c4c0b4e3dcdf77b674ac2b14a6686e
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-19 22:56:36 +0530

    Create methods.js

---
+++ Objects/methods.js
@@ -0,0 +1,13 @@
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+let alienShip = {
+  retreat() {
+    console.log(retreatMessage)
+  },
+  takeOff() {
+    console.log('Spim... Borp... Glix... Blastoff!')
+  }
+}
+
+alienShip.retreat()
+alienShip.takeOff()
