aeb29e826b008163bed5743c0c9af5b2bd6c65e0
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-19 15:19:45 +0530

    Create propertyAssignment.js

---
+++ Objects/propertyAssignment.js
@@ -0,0 +1,11 @@
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth',
+  color: 'silver',
+  'Secret Mission' : 'Discover life outside of Earth.'
+};
+
+spaceship.color = 'glorious gold'
+spaceship.numEngines = 2
+
+delete spaceship['Secret Mission']
