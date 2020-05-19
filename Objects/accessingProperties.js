287381cfde07ea047113005b1ea6a3da63ecbf6c
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-19 14:55:15 +0530

    Create accessingProperties.js

---
+++ Objects/accessingProperties.js
@@ -0,0 +1,10 @@
+let spaceship = {
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+let crewCount = spaceship.numCrew // crewCount = 5
+let planetArray = spaceship.flightPath 
