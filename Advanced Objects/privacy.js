6cf0096f669eada92310e824c9d135c04c31a19f
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-21 03:25:50 +0530

    Create privacy.js

---
+++ Advanced Objects/privacy.js
@@ -0,0 +1,13 @@
+const robot = {
+  _energyLevel: 100,
+  recharge(){
+    this._energyLevel += 30;
+    console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`)
+  }
+};
+
+robot._energyLevel = 'high'
+console.log(robot.recharge()) // Recharged! Energy is currently at high30%.undefined
+
+
+// Notice that a funky string is printed to the console! This is known as a side-effect of type-coercion. No need to worry about what this means for now, but it’s important to understand that you can cause unwanted side-effects when mutating objects and their properties.
