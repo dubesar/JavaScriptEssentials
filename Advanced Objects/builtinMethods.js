e1ee969d6ba9f713b1db714eb6e51bd55e7306ba
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-22 02:38:15 +0530

    Create builtinMethods.js

---
+++ Advanced Objects/builtinMethods.js
@@ -0,0 +1,20 @@
+const robot = {
+	model: 'SAL-1000',
+  mobile: true,
+  sentient: false,
+  armor: 'Steel-plated',
+  energyLevel: 75
+};
+
+const robotKeys = Object.keys(robot);
+
+console.log(robotKeys);
+
+// Declare robotEntries below this line:
+const robotEntries = Object.entries(robot)
+console.log(robotEntries);
+
+// Declare newRobot below this line:
+const newRobot = Object.assign({laserBlaster: true, voiceRecognition: true}, robot);
+
+console.log(newRobot);
