ab601d75159bd9700e86d291715ba7aa118c742e
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-22 00:06:19 +0530

    Create propertyValueShorthand.js

---
+++ Advanced Objects/propertyValueShorthand.js
@@ -0,0 +1,19 @@
+const robotFactory = (model, mobile) => {
+  return {
+    model,
+    mobile,
+    beep() {
+      console.log('Beep Boop');
+    }
+  }
+}
+
+// To check that the property value shorthand technique worked:
+const newRobot = robotFactory('P-501', false)
+console.log(newRobot.model)
+console.log(newRobot.mobile)
+
+/*
+In robotFactory there are a few instances where the property’s key and value share the same name, 
+you can remove the key : portion to destructure the object.
+*/
