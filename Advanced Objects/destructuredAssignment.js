8394595d3e7d65d862ea807451d8cabe1b90ecbf
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-22 02:29:24 +0530

    Create destructuredAssignment.js

---
+++ Advanced Objects/destructuredAssignment.js
@@ -0,0 +1,15 @@
+const robot = {
+  model: '1E78V2',
+  energyLevel: 100,
+  functionality: {
+    beep() {
+      console.log('Beep Boop');
+    },
+    fireLaser() {
+      console.log('Pew Pew');
+    },
+  }
+};
+
+const { functionality } = robot  //destructured assignment
+functionality.beep()   //using destructured assignment to call the method
