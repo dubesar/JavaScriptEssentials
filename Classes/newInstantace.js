4d59b85388cac308145c56227de3845f2a5f0beb
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-23 22:37:07 +0530

    Create newInstantace.js

---
+++ Classes/newInstantace.js
@@ -0,0 +1,10 @@
+class Surgeon {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+}
+
+const surgeonCurry = new Surgeon('Curry','Cardiovascular') // Create new Dog instance
+
+const surgeonDurant = new Surgeon('Durant','Orthopedics') // Create new Dog instance
