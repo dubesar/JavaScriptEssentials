2b2b44a52e917a77e0eea951aaae2fcff186183a
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-23 19:15:16 +0530

    Create introToClasses.js

---
+++ Classes/introToClasses.js
@@ -0,0 +1,24 @@
+class Dog {
+  constructor(name) {
+    this._name = name;
+    this._behavior = 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+  get behavior() {
+    return this._behavior;
+  }   
+
+  incrementBehavior() {
+    this._behavior ++;
+  }
+}
+
+const halley = new Dog('Halley');
+console.log(halley.name); // Print name value to console
+console.log(halley.behavior); // Print behavior value to console
+halley.incrementBehavior(); // Add one to behavior
+console.log(halley.name); // Print name value to console
+console.log(halley.behavior); // Print behavior value to console
