7b3d4e27c22012e1c346132673102a2cd915009f
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-25 14:24:38 +0530

    Create methodCalls.js

---
+++ Classes/methodCalls.js
@@ -0,0 +1,27 @@
+class Surgeon {
+  constructor(name, department) {
+    this._name = name;
+    this._department = department;
+    this._remainingVacationDays = 20;
+  }
+  get name() {
+    return this._name
+  }
+  get department() {
+    return this._department
+  }
+  get remainingVacationDays() {
+    return this._remainingVacationDays
+  }
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff
+  }
+}
+
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+
+console.log(surgeonCurry.name) //Curry
+surgeonCurry.takeVacationDays(3)
+
+console.log(surgeonCurry.remainingVacationDays) //17
