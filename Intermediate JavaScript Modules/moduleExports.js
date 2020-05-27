ca3bd94a79eeb266df9a545ebd86ed8ba81804ba
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-27 19:12:52 +0530

    Create moduleExports.js

---
+++ Intermediate JavaScript Modules/moduleExports.js
@@ -0,0 +1,11 @@
+const Airplane = {}
+
+Airplane.myAirplane = "StarJet"
+
+module.exports = Airplane
+
+/* The pattern we use to export modules is thus:
+
+1. Create an object to represent the module.
+2. Add properties or methods to the module object.
+3. Export the module with module.exports.*/
