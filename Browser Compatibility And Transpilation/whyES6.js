7ef9b99386090e0bda7908e4d411e7824c5fb35f
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-29 12:19:40 +0530

    Update whyES6.js

--- Browser Compatibility And Transpilation/whyES6.js
+++ Browser Compatibility And Transpilation/whyES6.js
@@ -1,8 +1,8 @@
 var pasta = "Spaghetti"; // ES5 syntax
 
-var meat = "Pancetta"; // ES6 syntax
+var meat = "Pancetta"; // ES6 syntax comnverted to ES5 using var
 
-var sauce = "Eggs and cheese"; // ES6 syntax
+var sauce = "Eggs and cheese"; // ES6 syntax converted to ES5 using var
 
-// Template literals, like the one below, were introduced in ES6
+// Template literals, like the one below, were introduced in ES6 and is converted to ES5 using + to add strings
 var carbonara = 'You can make carbonara with ' + pasta+', ' + meat+', ' + 'and a sauce made with ' + sauce + '.';
