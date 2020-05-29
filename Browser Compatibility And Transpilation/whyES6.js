887dcfd91e69d60b9773572762ca587664552b96
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-29 12:18:26 +0530

    Create whyES6.js

---
+++ Browser Compatibility And Transpilation/whyES6.js
@@ -0,0 +1,8 @@
+var pasta = "Spaghetti"; // ES5 syntax
+
+var meat = "Pancetta"; // ES6 syntax
+
+var sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+var carbonara = 'You can make carbonara with ' + pasta+', ' + meat+', ' + 'and a sauce made with ' + sauce + '.';
