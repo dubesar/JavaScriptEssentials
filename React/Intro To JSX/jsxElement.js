d9565c89031c9a90d170d7998f9078379578ccea
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-05-30 12:19:54 +0530

    Create jsxElement.js

---
+++ React/Intro To JSX/jsxElement.js
@@ -0,0 +1,14 @@
+/*
+A basic unit of JSX is called a JSX element.
+
+Here’s an example of a JSX element:
+
+<h1>Hello world</h1>
+
+This JSX element looks exactly like HTML! The only noticeable difference is that you would find it in a JavaScript file, instead of in an HTML file.
+*/
+
+// ---------------------------------------------------------------------------------------------------------------------------
+//Ex:
+
+<p>Hello World</p>
