5cb1c45d8d9b042832970d92d6cd67e2e5b61946
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-06-07 00:12:17 +0530

    Create jsxElementsandSurroundings.js

---
+++ React/Intro To JSX/jsxElementsandSurroundings.js
@@ -0,0 +1,23 @@
+/*
+
+JSX elements are treated as JavaScript expressions. They can go anywhere that JavaScript expressions can go.
+
+That means that a JSX element can be saved in a variable, passed to a function, stored in an object or array…you name it.
+
+Here’s an example of a JSX element being saved in a variable:
+
+const navBar = <nav>I am a nav bar</nav>;
+Here’s an example of several JSX elements being stored in an object:
+
+const myTeam = {
+  center: <li>Benzo Walli</li>,
+  powerForward: <li>Rasha Loa</li>,
+  smallForward: <li>Tayshaun Dasmoto</li>,
+  shootingGuard: <li>Colmar Cumberbatch</li>,
+  pointGuard: <li>Femi Billon</li>
+};
+
+*/
+
+// ------------------------------------------------Example--------------------------------------------------------------
+const myArticle = <article></article>
