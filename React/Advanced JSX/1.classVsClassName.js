61f2b2d0b63c28c40c604baa702036abd55a41b2
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-07-14 01:32:54 +0530

    Create 1.classVsClassName.js

---
+++ React/Advanced JSX/1.classVsClassName.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+const myDiv = (
+  <div className = 'big'>
+    I AM A BIG DIV
+  </div>
+)
+
+ReactDOM.render(myDiv,document.getElementById('app'))  //shows a big text on the screen
