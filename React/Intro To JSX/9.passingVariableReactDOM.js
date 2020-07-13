ccfd38284298f9a6607e47c3c95be3d752864472
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-07-14 01:15:22 +0530

    Create 9.passingVariableReactDOM.js

---
+++ React/Intro To JSX/9.passingVariableReactDOM.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+const myList = (
+  <ul>
+    <li>Sarvesh</li>
+    <li>Dubey</li>
+    <li>Engineer</li>
+  </ul>
+)
+
+ReactDOM.render(myList,document.getElementById('app'));
