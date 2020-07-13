acca8f47c00c31309959c5998f70a3387b0fc48f
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-07-13 11:21:07 +0530

    Create 5.outermostJSX.js

---
+++ React/Intro To JSX/5.outermostJSX.js
@@ -0,0 +1,17 @@
+
+//There’s a rule that we haven’t mentioned: a JSX expression must have exactly one outermost element.
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+const blog = (
+  <div>
+    <img src="pics/192940u73.jpg" />
+    <h1>
+      Welcome to Dan's Blog!
+    </h1>
+    <article>
+      Wow I had the tastiest sandwich today.  I <strong>literally</strong> almost freaked out.
+    </article>
+  </div>
+);
+   
