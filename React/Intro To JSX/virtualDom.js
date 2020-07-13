50c4d1b1f979aa75a34ec9d9290dc6c450b46ed1
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-07-14 01:26:02 +0530

    Create virtualDom.js

---
+++ React/Intro To JSX/virtualDom.js
@@ -0,0 +1,9 @@
+const hello = <h1>Hello world</h1>;
+
+// This will add "Hello world" to the screen:
+
+ReactDOM.render(hello, document.getElementById('app'));
+
+// This won't do anything at all:
+
+ReactDOM.render(hello, document.getElementById('app'));
