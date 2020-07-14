b1829b0e6c8e2d34b2133d1d8b62862afaffb026
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-07-15 00:36:44 +0530

    Create 2.selfClosingTags.js

---
+++ React/Advanced JSX/2.selfClosingTags.js
@@ -0,0 +1,13 @@
+const profile = (
+  <div>
+    <h1>I AM JENKINS</h1>
+    <img src="images/jenkins.png" />   //self-closing
+    <article>
+      I LIKE TO SIT
+      <br />                          //self-closing
+      JENKINS IS MY NAME
+      <br />                          //self-closing
+      THANKS HA LOT
+    </article>
+  </div>
+);
