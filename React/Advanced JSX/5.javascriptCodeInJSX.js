2baceb0afba790677dee03e5c5210bb3613407ee
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-07-15 00:56:10 +0530

    Update 5.javascriptCodeInJSX.js

--- React/Advanced JSX/5.javascriptCodeInJSX.js
+++ React/Advanced JSX/5.javascriptCodeInJSX.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 // Write code here:
 const math = (
-  <h1>2+3={2+3}</h1>
+  <h1>2+3={2+3}</h1>                                //renders 2+3=5 on the screen
 )
 
-ReactDOM.render(math,document.getElementById('app'))
+ReactDOM.render(math,document.getElementById('app'))  
