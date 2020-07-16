c914d14972330c7181c2ae9ac763862d020af63c
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-07-16 12:36:15 +0530

    Create 10.conditonalJSX.js

---
+++ React/Advanced JSX/10.conditonalJSX.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function coinToss() {
+  // This function will randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  kitty: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg'
+};
+let img;
+
+// if/else statement begins here:
+if (coinToss() === 'heads') {
+  img = (
+    <img src={pics.kitty} />
+  );
+} else {
+  img = ( 
+    <img src={pics.doggy} />
+  );
+}
+
+ReactDOM.render(img, document.getElementById('app'));
