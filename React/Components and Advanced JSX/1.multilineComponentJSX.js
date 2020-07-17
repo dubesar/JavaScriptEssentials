bf060b64988455313253eb98fde5ba6325353b37
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-07-18 00:08:31 +0530

    Create 1.multilineComponentJSX.js

---
+++ React/Components and Advanced JSX/1.multilineComponentJSX.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class QuoteMaker extends React.Component{
+  render() {
+    return (
+      <blockquote>
+      <p>
+        What is important now is to recover our senses.
+      </p>
+      <cite>
+        <a target="_blank" 
+          href="https://en.wikipedia.org/wiki/Susan_Sontag">
+          Susan Sontag
+        </a>
+      </cite>
+    </blockquote>
+    )
+  }
+};
