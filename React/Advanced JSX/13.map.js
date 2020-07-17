14babc84978dea3172ceeb64f9392aeaf5efa6bd
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-07-17 12:56:13 +0530

    Update 13.map.js

--- React/Advanced JSX/13.map.js
+++ React/Advanced JSX/13.map.js
@@ -1,5 +1,37 @@
-const strings = ['Home', 'Shop', 'About Me'];
 
-const listItems = strings.map(string => <li>{string}</li>);
+// Example
+// const strings = ['Home', 'Shop', 'About Me'];
+// const listItems = strings.map(string => <li>{string}</li>);
+// <ul>{listItems}</ul>
 
-<ul>{listItems}</ul>
+// This is fine in JSX, not in an explicit array:
+
+// <ul>
+//   <li>item 1</li>
+//   <li>item 2</li>
+//   <li>item 3</li>
+// </ul>
+
+// // This is also fine!
+
+// const liArray = [
+//   <li>item 1</li>, 
+//   <li>item 2<li>, 
+//   <li>item 3</li>
+// ];
+
+// <ul>{liArray}</ul>
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const people = ['Rowe', 'Prevost', 'Gare'];
+
+const peopleLis = people.map(person =>
+  // expression goes here:
+  <li>{person}</li>
+);
+
+// ReactDOM.render goes here:
+ReactDOM.render(<ul>{peopleLis}</ul>,document.getElementById('app'))
